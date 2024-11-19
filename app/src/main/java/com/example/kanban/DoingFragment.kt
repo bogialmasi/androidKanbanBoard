b568d195122ea8060d4e11691a5ba107f4be5dfa
bogialmasi <boglarkaalmasi@gmail.com>
2024-11-19 15:30:59 +0100

    add lists to todo and done fragments

--- app/src/main/java/com/example/kanban/DoingFragment.kt
+++ app/src/main/java/com/example/kanban/DoingFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 
 class DoingFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_doing, container, false)
+        val contentView = inflater.inflate(R.layout.fragment_doing, container, false)
+        val listView: ListView = contentView.findViewById(R.id.doing_list)
+        return contentView
     }
 }
--- app/src/main/java/com/example/kanban/DoneFragment.kt
+++ app/src/main/java/com/example/kanban/DoneFragment.kt
@@ -5,12 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+
 class DoneFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_done, container, false)
+        val contentView = inflater.inflate(R.layout.fragment_done, container, false)
+        val listView: ListView = contentView.findViewById(R.id.done_list)
+        return contentView
     }
 }
