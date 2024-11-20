7e16279f3bd96b738ae8cf75ee43df2c6044d809
bogialmasi <boglarkaalmasi@gmail.com>
2024-11-20 21:23:01 +0100

    putting list items in card view, initializing context menu

--- app/src/main/java/com/example/kanban/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/example/kanban/adapter/RecyclerAdapter.kt
@@ -1,28 +1,32 @@
 package com.example.kanban.adapter
 
 import android.content.Context
+import android.view.ContextMenu
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnCreateContextMenuListener
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kanban.R
 import com.example.kanban.model.KanbanItem
 
-class KanbanItemAdapter(val context: Context?, val dataSet: List<KanbanItem>) :
-    RecyclerView.Adapter<KanbanItemAdapter.ItemViewHolder>()
+class RecyclerAdapter(val context: Context?, val dataSet: List<KanbanItem>) :
+    RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder>(), OnCreateContextMenuListener
 
 {
     class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view){
         val textView: TextView = view.findViewById(R.id.list_item_content)
+        val cardView: CardView = view.findViewById(R.id.list_item_layout)
     }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KanbanItemAdapter.ItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ItemViewHolder {
         val adapterLayout = LayoutInflater.from(parent.context)
             .inflate(R.layout.list_item, parent, false)
         return ItemViewHolder(adapterLayout)
     }
 
-    override fun onBindViewHolder(holder: KanbanItemAdapter.ItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerAdapter.ItemViewHolder, position: Int) {
         val item = dataSet[position]
         holder.textView.text = item.content
     }
@@ -30,4 +34,13 @@ class KanbanItemAdapter(val context: Context?, val dataSet: List<KanbanItem>) :
     override fun getItemCount(): Int {
         return dataSet.size
     }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        TODO("Not yet implemented")
+    }
+
 }
