60c889198c55e4d34a55206aaafa320bd83ca817
bogialmasi <boglarkaalmasi@gmail.com>
2024-11-22 17:24:13 +0100

    eleted white space

--- app/src/main/java/com/example/kanban/activities/MainActivity.kt
+++ app/src/main/java/com/example/kanban/activities/MainActivity.kt
@@ -26,8 +26,6 @@ class MainActivity : AppCompatActivity() {
         val viewPagerAdapter = ViewPagerAdapter(this)
         viewPager.adapter = viewPagerAdapter
 
-
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
