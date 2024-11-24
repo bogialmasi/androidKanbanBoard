f440d731434bb1da9ee63e41da86ed3b83fe2c6f
bogialmasi <boglarkaalmasi@gmail.com>
2024-11-24 14:07:38 +0100

    .

--- app/src/main/java/com/example/kanban/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/example/kanban/adapter/RecyclerAdapter.kt
@@ -2,6 +2,7 @@ package com.example.kanban.adapter
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.media.MediaPlayer
 import android.view.ContextMenu
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +16,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.kanban.R
 import com.example.kanban.data.MyDBHelper
 import com.example.kanban.model.KanbanItem
+import com.google.android.material.animation.AnimationUtils
+import kotlin.concurrent.thread
 
 class RecyclerAdapter(val context: Context?, val dataSet: MutableList<KanbanItem>) :
     RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder>()
@@ -24,6 +27,7 @@ class RecyclerAdapter(val context: Context?, val dataSet: MutableList<KanbanItem
         val textView: TextView = view.findViewById(R.id.list_item_content)
         val progressBtn: Button = view.findViewById(R.id.list_item_progress_btn)
         val deleteButton: Button = view.findViewById(R.id.list_item_delete_btn)
+        val cardView: View = view.findViewById(R.id.list_item_layout)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ItemViewHolder {
         val adapterLayout = LayoutInflater.from(parent.context)
@@ -43,6 +47,7 @@ class RecyclerAdapter(val context: Context?, val dataSet: MutableList<KanbanItem
             notifyItemRangeChanged(position, dataSet.size - position)
             notifyDataSetChanged()
             if(item.state == 3 ){
+                MyDBHelper(context).deleteKanbanItem(item.id)
                 Toast.makeText(context, context!!.getString(R.string.toast_progress_over3), Toast.LENGTH_LONG).show()
             }
             else {
